5246f7224b1d3b99dd8d084ec3d92b28fbc6a162
Rivas12 <rivaldosilveira13@gmail.com>
2025-06-03 15:44:07 -0300

    fix: correct import path syntax for termos.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import '../styles/termos.css';
+import '@/styles/termos.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
